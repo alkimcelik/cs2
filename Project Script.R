ceeaa1d920d39b2b360b8a7f678daa9a4493d584
alkimcelik <alkimcancelik33@gmail.com>
2023-05-23 17:06:43 +0200

    Update Project Script.R

--- Project Script.R
+++ Project Script.R
@@ -1,3 +1,13 @@
+calc_AIC <- function(model){
+  log_likelihood <- model@fit$LLH
+  
+  # Calculate the number of parameters
+  num_parameters <- length(model@fit$coef)
+  
+  # Calculate the AIC
+  aic <- -2 * log_likelihood + 2 * num_parameters
+  return(aic)
+}
 commodities <- read.csv('C:/Users/alkim/OneDrive/Documents/GitHub/cs2/commodities.csv')
 commodities$date <- as.Date(commodities$date)
 ####Plotting the time series basic####
@@ -28,25 +38,113 @@ pacf(commodities$NGas**2)
 install.packages("rugarch")
 library(rugarch)
 
-# Create a time series object from your data
-# Replace 'your_data' with your actual time series data
-ts_data <- ts(your_data)
+### Train datalarini cikardik
+NGas_ts <- ts(commodities$NGas, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
+oil_ts <- ts(commodities$oil, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
+coal_ts <- ts(commodities$coal, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
+NGas_ts_train <- as.ts(NGas_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
+oil_ts_train <- as.ts(oil_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
+coal_ts_train <- as.ts(coal_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
+NGas_ts_test <- as.ts(NGas_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
+oil_ts_test <- as.ts(oil_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
+coal_ts_test <- as.ts(coal_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
 
 # Specify the ARMA(p,q) and GARCH(r,s) orders
-p <- 1  # Autoregressive order
-q <- 1  # Moving average order
-r <- 1  # GARCH order
-s <- 1  # GARCH order for lagged conditional variances
+p <- c(0:2)  # Autoregressive order
+q <- c(0:2)  # Moving average order
+r <- c(0:2)  # GARCH order
+s <- c(0:2)  # GARCH order for lagged conditional variances
 
+####Models##########
 # Specify the specification for the ARMA-GARCH model
-spec <- ugarchspec(mean.model = list(armaOrder = c(p, q)),
-                   variance.model = list(garchOrder = c(r, s)))
+AIC_values_NGas <- list()
+AIC_values_oil <- list()
+AIC_values_coal <- list()
+counter <- 1
+for(i in p){
+  for(j in q){
+    for(k in r){
+      for(l in s){
+        if(k == 0 & l == 0){
+          next
+        }
+        else{
+          spec <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
+                             variance.model = list(garchOrder = c(k, l)))
+          fit_NGas <- ugarchfit(spec, NGas_ts_train, solver = 'hybrid')
+          fit_oil <- ugarchfit(spec, oil_ts_train, solver = 'hybrid')
+          fit_coal <- ugarchfit(spec, coal_ts_train, solver = 'hybrid')
+          AIC_values_NGas[[counter]] <- calc_AIC(fit_NGas)
+          AIC_values_oil[[counter]] <- calc_AIC(fit_oil)
+          AIC_values_coal[[counter]] <- calc_AIC(fit_coal)
+          index_list[[counter]] <- paste(i,j,k,l)
+          counter <- counter + 1
+          print(paste(i,j,k,l))
+        }
+      }
+    }
+  }
+}
+AIC_values_index <- as.data.frame(cbind(unlist(index_list),unlist(AIC_values_NGas),unlist(AIC_values_oil),unlist(AIC_values_coal)))
+##Best model with parameters p=2, q=1, r=1, s=1
+best_model_NGas <- ugarchspec(mean.model = list(armaOrder = c(2, 1)),
+                                           variance.model = list(garchOrder = c(1, 1)))
+best_model_NGas_fit <- ugarchfit(best_model_NGas, NGas_ts_train, solver = 'hybrid')
 
-# Fit the ARMA-GARCH model to the data
-fit <- ugarchfit(spec, ts_data)
+####Coal Model##########
+# Specify the specification for the ARMA-GARCH model
+AIC_values <- list()
+index_list <- list()
+counter <- 1
+for(i in p){
+  for(j in q){
+    for(k in r){
+      for(l in s){
+        if(k == 0 & l == 0){
+          next
+        }
+        else{
+          spec <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
+                             variance.model = list(garchOrder = c(k, l)))
+          fit <- ugarchfit(spec, coal_ts_train, solver = 'hybrid')
+          AIC_values[[counter]] <- calc_AIC(fit)#Akaike
+          index_list[[counter]] <- paste(i,j,k,l)
+          counter <- counter + 1
+          print(paste(i,j,k,l))
+        }
+      }
+    }
+  }
+}
+AIC_values_index <- as.data.frame(cbind(unlist(AIC_values), unlist(index_list)))
+best_model_coal <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
+                              variance.model = list(garchOrder = c(k, l)))
+best_model_coal_fit <- ugarchfit(best_model_coal, coal_ts_train, solver = 'hybrid')
 
-# Print the model summary
-show(fit)
+####oil Model##########
+# Specify the specification for the ARMA-GARCH model
+AIC_values <- list()
+index_list <- list()
+counter <- 1
+for(i in p){
+  for(j in q){
+    for(k in r){
+      for(l in s){
+          spec <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
+                             variance.model = list(garchOrder = c(k, l)))
+          fit <- ugarchfit(spec, oil_ts_train, solver = 'hybrid')
+          AIC_values[[counter]] <- calc_AIC(fit)#Akaike
+          index_list[[counter]] <- paste(i,j,k,l)
+          counter <- counter + 1
+          print(paste(i,j,k,l))
+      }
+    }
+  }
+}
+
+best_model_oil <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
+                              variance.model = list(garchOrder = c(k, l)))
+best_model_oil_fit <- ugarchfit(best_model_oil, oil_ts_train, solver = 'hybrid')
 
 # Access the estimated parameters
 estimated_params <- coef(fit)
