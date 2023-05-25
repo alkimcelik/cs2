c13990bcbbbe1446d1cfee9bbfd07223d0ad9bfb
alkimcelik <alkimcancelik33@gmail.com>
2023-05-26 00:01:47 +0200

    Update Project Script.R

--- Project Script.R
+++ Project Script.R
@@ -8,6 +8,11 @@ calc_AIC <- function(model){
   aic <- -2 * log_likelihood + 2 * num_parameters
   return(aic)
 }
+CRPS <- function(x,mean,sigma){
+  standart_value <- as.numeric((x-mean)/sigma)
+  score <- sigma*(1/sqrt(pi)-2*dnorm(standart_value)-standart_value*(2*pnorm(standart_value)-1))
+  return(score)
+}
 commodities <- read.csv('C:/Users/alkim/OneDrive/Documents/GitHub/cs2/commodities.csv')
 commodities$date <- as.Date(commodities$date)
 ####Plotting the time series basic####
@@ -35,19 +40,22 @@ pacf(commodities$NGas**2)
 
 #PART B (you know from where lol)
 # Install and load the 'rugarch' package
-install.packages("rugarch")
 library(rugarch)
 
+#part b
+
+# Create the xts object
+xts_obj <- xts(commodities$NGas, order.by = commodities$date)
 ### Train datalarini cikardik
-NGas_ts <- ts(commodities$NGas, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
-oil_ts <- ts(commodities$oil, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
-coal_ts <- ts(commodities$coal, start = c(2010, 03, 16), end = c(2020, 10, 27), freq = 365)
-NGas_ts_train <- as.ts(NGas_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
-oil_ts_train <- as.ts(oil_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
-coal_ts_train <- as.ts(coal_ts[1:2500], start = c(2010, 03, 16), end = c(2020, 01, 17),freq = 365)
-NGas_ts_test <- as.ts(NGas_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
-oil_ts_test <- as.ts(oil_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
-coal_ts_test <- as.ts(coal_ts[1:2500], start = c(2020, 01, 18), end = c(2020, 10, 27),freq = 365)
+NGas_ts <- xts(commodities$NGas, order.by = commodities$date)
+oil_ts <- xts(commodities$oil, order.by = commodities$date)
+coal_ts <- xts(commodities$coal, order.by = commodities$date)
+NGas_ts_train <- NGas_ts[1:2500]
+oil_ts_train <- oil_ts[1:2500]
+coal_ts_train <- coal_ts[1:2500]
+NGas_ts_test <- NGas_ts[2501:2700]
+oil_ts_test <- oil_ts[2501:2700]
+coal_ts_test <- coal_ts[2501:2700]
 
 # Specify the ARMA(p,q) and GARCH(r,s) orders
 p <- c(0:2)  # Autoregressive order
@@ -60,6 +68,7 @@ s <- c(0:2)  # GARCH order for lagged conditional variances
 AIC_values_NGas <- list()
 AIC_values_oil <- list()
 AIC_values_coal <- list()
+index_list <- list()
 counter <- 1
 for(i in p){
   for(j in q){
@@ -86,69 +95,97 @@ for(i in p){
   }
 }
 AIC_values_index <- as.data.frame(cbind(unlist(index_list),unlist(AIC_values_NGas),unlist(AIC_values_oil),unlist(AIC_values_coal)))
+AIC_values_index$V2 <- as.numeric(AIC_values_index$V2) 
+AIC_values_index$V3<- as.numeric(AIC_values_index$V3) 
+AIC_values_index$V4<- as.numeric(AIC_values_index$V4) 
 ##Best model with parameters p=2, q=1, r=1, s=1
 best_model_NGas <- ugarchspec(mean.model = list(armaOrder = c(2, 1)),
                                            variance.model = list(garchOrder = c(1, 1)))
 best_model_NGas_fit <- ugarchfit(best_model_NGas, NGas_ts_train, solver = 'hybrid')
 
-####Coal Model##########
-# Specify the specification for the ARMA-GARCH model
-AIC_values <- list()
-index_list <- list()
-counter <- 1
-for(i in p){
-  for(j in q){
-    for(k in r){
-      for(l in s){
-        if(k == 0 & l == 0){
-          next
-        }
-        else{
-          spec <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
-                             variance.model = list(garchOrder = c(k, l)))
-          fit <- ugarchfit(spec, coal_ts_train, solver = 'hybrid')
-          AIC_values[[counter]] <- calc_AIC(fit)#Akaike
-          index_list[[counter]] <- paste(i,j,k,l)
-          counter <- counter + 1
-          print(paste(i,j,k,l))
-        }
-      }
-    }
-  }
-}
-AIC_values_index <- as.data.frame(cbind(unlist(AIC_values), unlist(index_list)))
-best_model_coal <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
-                              variance.model = list(garchOrder = c(k, l)))
-best_model_coal_fit <- ugarchfit(best_model_coal, coal_ts_train, solver = 'hybrid')
-
-####oil Model##########
-# Specify the specification for the ARMA-GARCH model
-AIC_values <- list()
-index_list <- list()
-counter <- 1
-for(i in p){
-  for(j in q){
-    for(k in r){
-      for(l in s){
-          spec <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
-                             variance.model = list(garchOrder = c(k, l)))
-          fit <- ugarchfit(spec, oil_ts_train, solver = 'hybrid')
-          AIC_values[[counter]] <- calc_AIC(fit)#Akaike
-          index_list[[counter]] <- paste(i,j,k,l)
-          counter <- counter + 1
-          print(paste(i,j,k,l))
-      }
-    }
-  }
-}
-
-best_model_oil <- ugarchspec(mean.model = list(armaOrder = c(i, j)),
-                              variance.model = list(garchOrder = c(k, l)))
+best_model_oil <- ugarchspec(mean.model = list(armaOrder = c(0, 1)),
+                              variance.model = list(garchOrder = c(1, 1)))
 best_model_oil_fit <- ugarchfit(best_model_oil, oil_ts_train, solver = 'hybrid')
 
+best_model_coal <- ugarchspec(mean.model = list(armaOrder = c(2, 2)),
+                              variance.model = list(garchOrder = c(1, 1)))
+best_model_coal_fit <- ugarchfit(best_model_coal, coal_ts_train, solver = 'hybrid')
+
+
 # Access the estimated parameters
-estimated_params <- coef(fit)
-print(estimated_params)
+print('NGas (2,1,1,1):')
+coef(best_model_NGas_fit)
+print('oil (0,1,1,1):')
+coef(best_model_oil_fit)
+print('coal (2,2,1,1):')
+coef(best_model_coal_fit)
+#part c
+best_model_NGas_fit_sigma <- xts(sigma(best_model_NGas_fit)**2, order.by = commodities$date[1:2500])
+plot.xts(best_model_NGas_fit_sigma)
+best_model_oil_fit_sigma <- xts(sigma(best_model_oil_fit)**2, order.by = commodities$date[1:2500])
+plot.xts(best_model_oil_fit_sigma)
+best_model_coal_fit_sigma <- xts(sigma(best_model_coal_fit)**2, order.by = commodities$date[1:2500])
+plot.xts(best_model_coal_fit_sigma)
+#part d
+
+######ONE SHOT FORECASTING################
+forecast_NGas <- ugarchforecast(best_model_NGas_fit, n.ahead = 200)
+fitted_forecast_NGas <- fitted(forecast_NGas)
+sigma_forecast_NGas <- sigma(forecast_NGas)
+
+forecast_oil <- ugarchforecast(best_model_oil_fit, n.ahead = 200)
+fitted_forecast_oil <- fitted(forecast_oil)
+sigma_forecast_oil <- sigma(forecast_oil)
+
+
+forecast_coal <- ugarchforecast(best_model_coal_fit, n.ahead = 200)
+fitted_forecast_coal <- fitted(forecast_coal)
+sigma_forecast_coal <- sigma(forecast_coal)
+CRPS_NGas <- list()
+CRPS_oil <- list()
+CRPS_coal <- list()
+for(i in 1:200){
+  CRPS_NGas[[i]] <- CRPS(forecast_NGas@forecast$seriesFor[i], coef(best_model_NGas_fit)[1], sigma(best_model_NGas_fit)[1])
+  CRPS_oil[[i]] <- CRPS(forecast_oil@forecast$seriesFor[i], coef(best_model_oil_fit)[1], sigma(best_model_oil_fit)[1])
+  CRPS_coal[[i]] <- CRPS(forecast_coal@forecast$seriesFor[i], coef(best_model_coal_fit)[1], sigma(best_model_coal_fit)[1])
+}
+
+#####ITERATIVE FORECASTING WITH REESTIMATION######
+fitted_forecast_NGas <- list()
+mu_forecast_NGas <- list()
+sigma_forecast_NGas <- list()
+fitted_forecast_oil <- list()
+mu_forecast_oil <- list()
+sigma_forecast_oil <- list()
+fitted_forecast_coal <- list()
+mu_forecast_coal <- list()
+sigma_forecast_coal <- list()
+
+#for (i in 1:(length(NGas_ts) - 2500)){
+for (i in 1:200){
+  best_model_NGas_fit <- ugarchfit(best_model_NGas, NGas_ts[1:(2500+i-1)], solver = 'hybrid')
+  forecast_NGas <- ugarchforecast(best_model_NGas_fit, n.ahead = 1)
+  fitted_forecast_NGas[[i]] <- fitted(forecast_NGas)
+  sigma_forecast_NGas[[i]] <- sigma(forecast_NGas)
+  
+  best_model_oil_fit <- ugarchfit(best_model_oil, oil_ts[1:(2500+i-1)], solver = 'hybrid')
+  forecast_oil <- ugarchforecast(best_model_oil_fit, n.ahead = 1)
+  fitted_forecast_oil[[i]] <- fitted(forecast_oil)
+  sigma_forecast_oil[[i]] <- sigma(forecast_oil)
+  
+  best_model_coal_fit <- ugarchfit(best_model_coal, coal_ts[1:(2500+i-1)], solver = 'hybrid')
+  forecast_coal <- ugarchforecast(best_model_coal_fit, n.ahead = 1)
+  fitted_forecast_coal[[i]] <- fitted(forecast_coal)
+  sigma_forecast_coal[[i]] <- sigma(forecast_coal)
+  
+  CRPS_NGas[[i]] <- CRPS(forecast_NGas@forecast$seriesFor, coef(best_model_NGas_fit)[1], sd(NGas_ts[1:(2500+i-1)]))
+  CRPS_oil[[i]] <- CRPS(forecast_oil@forecast$seriesFor, coef(best_model_oil_fit)[1], sd(oil_ts[1:(2500+i-1)]))
+  CRPS_coal[[i]] <- CRPS(forecast_coal@forecast$seriesFor, coef(best_model_coal_fit)[1], sd(coal_ts[1:(2500+i-1)]))
+  #print(paste0('%',(i/(length(NGas_ts) - 2500))*100))
+  print(paste0('%',i/2))
+}
+
+
 
 #CRPS
 # Create a vector of observed values
